1bae796013a7c28926afe79c9c6cd72f261b71ac
omidfarhangnia <83111084+omidfarhangnia@users.noreply.github.com>
2022-10-15 17:03:27 +0330

    adding json datas to the js file

---
+++ main.js
@@ -0,0 +1,14 @@
+var ServerData = `
+[
+    {"SongName" : "Adagio Con Amore" , "SingerName" : "null" , isFavorite : false},
+    {"SongName" : "Butterfly Waltz" , "SingerName" : "null" , isFavorite : true},
+    {"SongName" : "Dream of Dreams" , "SingerName" : "null" , isFavorite : true},
+    {"SongName" : "Italian Summer" , "SingerName" : "null" , isFavorite : false},
+    {"SongName" : "Largo Maestoso" , "SingerName" : "null" , isFavorite : false},
+    {"SongName" : "Moonlit Shore" , "SingerName" : "null" , isFavorite : true},
+    {"SongName" : "Rain" , "SingerName" : "null" , isFavorite : false},
+    {"SongName" : "Solitary Hill" , "SingerName" : "null" , isFavorite : false},
+    {"SongName" : "Time Forgotten" , "SingerName" : "null" , isFavorite : true},
+    {"SongName" : "Wind" , "SingerName" : "null" , isFavorite : false}
+]
+`;
